aa5012aa335e742722c289eb24b7d2c2fb1f3a0b
heronsousa <heronrs1@gmail.com>
2020-05-13 13:17:41 -0300

    decrypting message

---
+++ criptografia.R
@@ -0,0 +1,22 @@
+library('httr')
+library("stringr")
+
+url = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data"
+
+response <- GET(url, query = list(token = '0aea0ad17e72ed84d2eabbf2d04ea3caa3e86c5a'))
+data <- content(response, type="application/json")
+
+alfabeto <- c('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+
+casas <- unlist(data["numero_casas"], use.names=F)
+cifra <- str_to_lower(unlist(data["cifrado"], use.names=F))
+
+result = ''
+
+for(c in strsplit(cifra, "")[[1]]) {
+  if(!is.na(match(c, alfabeto))) {
+    result <- str_c(result, alfabeto[((match(c, alfabeto)-casas)%%26)])
+  } else {
+    result <- str_c(result, c)
+  }
+}
