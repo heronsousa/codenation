77d99f72ecbc52be2c076f0c277520ea30ec1276
Heron Rodrigues <heronrs1@gmail.com>
2020-07-01 21:03:30 -0300

    Update fourth week challenge

--- csharp-7/Source/Models/CodenationContext.cs
+++ csharp-7/Source/Models/CodenationContext.cs
@@ -5,6 +5,11 @@ namespace Codenation.Challenge.Models
     public class CodenationContext : DbContext
     {
         public DbSet<Company> Companies { get; set; }
+        public DbSet<Candidate> Candidates { get; set; }
+        public DbSet<Acceleration> Accelerations { get; set; }
+        public DbSet<Challenge> Challenges { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Submission> Submissions { get; set; }
                 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
