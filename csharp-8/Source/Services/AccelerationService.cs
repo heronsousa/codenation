24e8dc02e9f773fd9bc184166ea3e33e67607836
Heron Rodrigues <heronrs1@gmail.com>
2020-07-06 20:51:32 -0300

    Update acceleration and user services

--- csharp-8/Source/Services/AccelerationService.cs
+++ csharp-8/Source/Services/AccelerationService.cs
@@ -1,27 +1,41 @@
 using System.Collections.Generic;
+using System.Linq;
 using Codenation.Challenge.Models;
 
 namespace Codenation.Challenge.Services
 {
     public class AccelerationService : IAccelerationService
     {
+        private CodenationContext _context;
+
         public AccelerationService(CodenationContext context)
         {
+            _context = context;
         }
 
         public IList<Acceleration> FindByCompanyId(int companyId)
         {
-            throw new System.NotImplementedException();
+            return _context.Candidates
+                .Where(c => c.CompanyId == companyId)
+                .Select(c => c.Acceleration)
+                .Distinct()
+                .ToList();
         }
 
         public Acceleration FindById(int id)
         {
-            throw new System.NotImplementedException();
+            return _context.Accelerations.Find(id);
         }
 
         public Acceleration Save(Acceleration acceleration)
         {
-            throw new System.NotImplementedException();
+            if (acceleration.Id == 0)
+                _context.Add(acceleration);
+            else
+                _context.Update(acceleration);
+                _context.SaveChanges();
+
+            return acceleration;
         }
     }
 }
--- csharp-8/Source/Services/UserService.cs
+++ csharp-8/Source/Services/UserService.cs
@@ -1,32 +1,48 @@
 using System.Collections.Generic;
+using System.Linq;
 using Codenation.Challenge.Models;
 
 namespace Codenation.Challenge.Services
 {
     public class UserService : IUserService
     {
+        private CodenationContext _context;
+
         public UserService(CodenationContext context)
         {
+            _context = context;
         }
 
         public IList<User> FindByAccelerationName(string name)
         {
-            throw new System.NotImplementedException();
+            return _context.Candidates
+                .Where(c => c.Acceleration.Name == name)
+                .Select(c => c.User)
+                .ToList();
         }
 
         public IList<User> FindByCompanyId(int companyId)
         {
-            throw new System.NotImplementedException();
+            return _context.Candidates
+               .Where(c => c.CompanyId == companyId)
+               .Select(c => c.User)
+               .ToList();
         }
 
         public User FindById(int id)
         {
-            throw new System.NotImplementedException();
+            return _context.Users.Find(id);
         }
 
         public User Save(User user)
         {
-            throw new System.NotImplementedException();
+            if (user.Id == 0)
+                _context.Add(user);
+            else
+                _context.Update(user);
+                _context.SaveChanges();
+
+            return user;
         }
     }
 }
